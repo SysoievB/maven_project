5c19afcffe84f62c4f0efaaab862246597b24086
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-16 10:17:20 +0200

    Added pdf cheat sheet

---
+++ src/main/java/com/maven/User.java
@@ -0,0 +1,18 @@
+package com.maven;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    String name;
+    int age;
+}
