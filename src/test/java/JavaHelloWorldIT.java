f3b7e10e99d9c0936181acf7fe607bbad38b6cfe
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-20 05:31:03 +0200

    Added plugin for running tests

---
+++ src/test/java/JavaHelloWorldIT.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("--------------My IT Ran--------------");
+    }
+}
